0d807206603e793d5e5404c1e312a1a60549d535
RaY8118 <parthghadi43@gmail.com>
2025-08-30 23:21:43 +0530

    feat: Add UDP sender

---
+++ cmd/udpsender/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	addr, err := net.ResolveUDPAddr("udp", ":42069")
+	if err != nil {
+		log.Fatalf("failed to resovle addr: %v", err)
+	}
+
+	conn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		log.Fatalf("failed to dial udp: %v", err)
+	}
+
+	defer conn.Close()
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			log.Printf("error reading input: %v", err)
+			continue
+		}
+
+		_, err = conn.Write([]byte(line))
+		if err != nil {
+			fmt.Printf("error writing udp: %v", err)
+			continue
+		}
+	}
+}
+
