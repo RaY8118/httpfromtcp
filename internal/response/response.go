5079ef587b40adae533852e2a5e7e46f3bcd4207
RaY8118 <parthghadi43@gmail.com>
2025-09-12 19:12:52 +0530

    refactor: improve http response writing

--- internal/response/response.go
+++ internal/response/response.go
@@ -7,14 +7,23 @@ import (
 	"ray8118/httpfromtcp/internal/request"
 )
 
+type Response struct {
+}
+
+type Writer struct {
+	writer io.Writer
+}
+
+func NewWriter(writer io.Writer) *Writer {
+	return &Writer{writer: writer}
+}
+
 type HandlerError struct {
 	StatusCode StatusCode
 	Message    string
 }
 
 type Handler func(w io.Writer, req *request.Request) *HandlerError
-type Response struct {
-}
 
 type StatusCode int
 
@@ -33,19 +42,7 @@ func GetDefaultHeaders(contentLen int) *headers.Headers {
 	return h
 }
 
-func WriteHeaders(w io.Writer, h *headers.Headers) error {
-	var err error = nil
-	b := []byte{}
-	h.ForEach(func(n, v string) {
-		b = fmt.Appendf(b, "%s: %s\r\n", n, v)
-	})
-	b = fmt.Append(b, "\r\n")
-	_, err = w.Write(b)
-	return err
-
-}
-
-func WriteStatusLine(w io.Writer, statusCode StatusCode) error {
+func (w *Writer) WriteStatusLine(statusCode StatusCode) error {
 	statusLine := []byte{}
 	switch statusCode {
 	case StatusOk:
@@ -58,6 +55,22 @@ func WriteStatusLine(w io.Writer, statusCode StatusCode) error {
 		return fmt.Errorf("unrecognized error code")
 	}
 
-	_, err := w.Write(statusLine)
+	_, err := w.writer.Write(statusLine)
 	return err
+}
+
+func (w *Writer) WriteHeaders(h headers.Headers) error {
+	b := []byte{}
+	h.ForEach(func(n, v string) {
+		b = fmt.Appendf(b, "%s: %s\r\n", n, v)
+	})
+	b = fmt.Append(b, "\r\n")
+	_, err := w.writer.Write(b)
+	return err
+
+}
+func (w *Writer) WriteBody(p []byte) (int, error) {
+	n, err := w.writer.Write(p)
+
+	return n, err
 }
